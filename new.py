4f958a8876f49dd211d771fd1dd0587477dcbac1
Atyam Aravind <113358558+atyamaravind@users.noreply.github.com>
2022-10-25 11:59:49 +0530

    version 1..

---
+++ new.py
@@ -0,0 +1,57 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+3
+5
+2+536
+538
+58963-96
+58867
+8*9
+72
+8/4
+2.0
+66/3
+22.0
+5//2
+2
+8+9-10
+7
+8+9-
+SyntaxError: incomplete input
+
+>>> 8+2*3
+14
+>>> (8+2)*3
+30
+>>> 2**3
+8
+>>> 2**10
+1024
+>>> 6**9
+10077696
+>>> 10/2
+5.0
+>>> 10/3
+3.3333333333333335
+>>> 10//3
+3
+>>> 10%3
+1
+>>> "navin"
+'navin'
+>>> print("fasak")
+fasak
+>>> print("aravind")
+aravind
+]
+>>> print("navin's laptop")
+navin's laptop
+>>> print('navin"slaptop')
+navin"slaptop
+>>> print('navin\'s "laptop"')
+navin's "laptop"
+>>> 100*"aravind"
+'aravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravindaravind'
+>>> print("c:\docs\n navin")
+c:\docs
+ navin
---
+++ prac.py
@@ -0,0 +1,79 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=2
+x+3
+5
+y=3
+x+y
+5
+x=9
+x+y
+12
+x
+9
+abc
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    abc
+NameError: name 'abc' is not defined. Did you mean: 'abs'?
+
+x-1
+8
+x+10
+19
+19+y
+22
+-+y
+-3
+_+y
+0
+name="youtube
+SyntaxError: incomplete input
+name"youtube"
+SyntaxError: invalid syntax
+>>> name="youtube
+SyntaxError: incomplete input
+>>> name=youtube"
+SyntaxError: incomplete input
+>>> name='youtube'
+>>> 
+>>> n[0]
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    n[0]
+NameError: name 'n' is not defined
+>>> name='youtube'
+>>> name[0]
+'y'
+>>> name[-1]
+'e'
+>>> name[0:2
+... 
+... 
+... 
+... 
+...      name[0:2]
+...      
+SyntaxError: '[' was never closed
+>>> name="youtube['
+...      
+SyntaxError: incomplete input
+>>> name="youtube'
+...      
+SyntaxError: incomplete input
+>>> name='youtube'
+...      
+>>> n=[1:]
+...      
+SyntaxError: invalid syntax
+>>> name[1:]
+...      
+'outube'
+>>> 'my'+name[3:]
+...      
+'mytube'
+>>> myname="Aravind"
+...      
+>>> len(myname)
+...      
+7
